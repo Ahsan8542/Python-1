03a595e987749d2ee05520eb3f987d516b3d39d5
Ahsan8542 <ahsanaligsk123@gmail.com>
2026-07-11 21:55:10 +0500

    x

---
+++ Calculator.py
@@ -0,0 +1,22 @@
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    return x / y
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("Sum :", add(x, y))
+print("Difference :", subtract(x, y))
+print("Product :", multiply(x, y))
+print("Quotient :", divide(x, y))
